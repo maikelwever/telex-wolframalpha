6f77b98308e43d035839e9e0b37819289d643e9f
Maikel Wever <maikelwever@gmail.com>
2015-06-11 19:37:44 +0200

    Result only is boring, show all info always :D

--- wolfram.py
+++ wolfram.py
@@ -24,14 +24,12 @@ class WolframAlphaPlugin(plugin.TelexPlugin):
         question = matches.group(1)
         client = wolframalpha.Client(self.read_option('api_key'))
         res = client.query(question)
-        try:
-            result_text = next(res.results).text
-        except StopIteration:
-            if not len(res.pods):
-                result_text = "No results :("
-            else:
-                result_text = "\n".join("* {0}:\n    - {1}".format(i.title, "\n    - ".join(i.text.split("\n")))
-                                        for i in res.pods if i.text)
+
+        if not len(res.pods):
+            result_text = "No results :("
+        else:
+            result_text = "\n".join("* {0}:\n    - {1}".format(i.title, "\n    - ".join(i.text.split("\n")))
+                                    for i in res.pods if i.text)
 
         result_text += "\nPowered by http://wolframalpha.com/input/?i=" + quote(question, safe='')
         return result_text
